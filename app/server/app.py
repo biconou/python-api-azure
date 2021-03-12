e4a1640d5f563fe0e2ed152822455bd4771d8f04
Yoann Couble <ycouble@palo-it.com>
2021-03-12 17:18:50 +0100

    modified structure of project

--- app/server/app.py
+++ app/server/app.py
@@ -1,10 +1,6 @@
-import logging
-
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
-logger = logging.getLogger()
 
 
 @app.get("/", tags=["Root"])
@@ -18,8 +14,4 @@ async def get_customer(customer_id):
 
 @app.post("/customer/{customer_id}")
 async def post_customer(customer_id):
-    logger.info("Adding a customer")
     return {"CustomerID": customer_id}
-
-if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
