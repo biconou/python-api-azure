3b24a1ada290cc328b038abc61b806a6f0d0fcd3
remi <rcocula@gmail.com>
2021-03-17 14:26:28 +0100

    user python3 command

--- startup.sh
+++ startup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # Install Poetry
-curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 poetry_exe=$HOME/.poetry/bin/poetry
 
 # Install dependencies
