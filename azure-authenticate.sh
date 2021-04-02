e6ac5a509b0f801f978c131ac6f5bb4c2e0dd4f3
Yoann Couble <ycouble@palo-it.com>
2021-04-02 10:59:57 +0200

    fix for application ID

--- azure-authenticate.sh
+++ azure-authenticate.sh
@@ -4,10 +4,12 @@
 
 echo ${clientId}
 
-response=$(curl --location --request POST 'https://login.microsoftonline.com/eadd0adf-e413-414f-a21a-86169869dcb1/oauth2/v2.0/token' \
+applicationId="0077cc8c-b2a3-4ec0-bd91-784fdf4f5f3d"
+
+response=$(curl --location --request POST "https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token" \
     --header 'Content-Type: application/x-www-form-urlencoded' \
     --data-urlencode "client_id=${clientId}" \
-    --data-urlencode 'scope=user.read openid profile offline_access' \
+    --data-urlencode "scope=${applicationId}/.default" \
     --data-urlencode "client_secret=${clientSecret}" \
     --data-urlencode 'username=DROP-5EFF56A2AF15@everie.com' \
     --data-urlencode 'password=g5fQO3NgOwz0Wnab2pRv' \
@@ -25,4 +27,3 @@ curl -X 'GET' \
   'https://drop-api-master.azurewebsites.net/pschit/' \
   --header "Authorization: Bearer ${access_token}" \
   --header 'accept: application/json'
-
